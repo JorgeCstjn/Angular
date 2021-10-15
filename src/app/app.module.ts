0a8fcfad7805c9ed7947e27d35c8e0605e2ed0e2
JorgeCstjn <73123281+JorgeCstjn@users.noreply.github.com>
2021-10-15 16:59:09 +0200

    commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ContadorComponent } from './contador/contador.component';
+import { HeroeComponent } from './Heroes/Heroe/heroe.component';
+import { ListadoComponent } from './listado/listado/listado.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContadorComponent,
+    HeroeComponent,
+    ListadoComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [HeroeComponent, ContadorComponent, ListadoComponent]
+})
+export class AppModule { }
---
+++ src/app/listado/listado/listado.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+  styleUrls: ['./listado.component.css']
+})
+export class ListadoComponent {
+
+  heroes: string[] = ['Spiderman', 'Superman', 'Batman', 'Ironman'];
+
+}
+
