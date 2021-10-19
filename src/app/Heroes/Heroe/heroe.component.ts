341cc4622fd94e1105918623d41cfa420a5adc61
JorgeCstjn <73123281+JorgeCstjn@users.noreply.github.com>
2021-10-19 18:51:40 +0200

    Ejercicio

--- src/app/Heroes/Heroe/heroe.component.ts
+++ src/app/Heroes/Heroe/heroe.component.ts
@@ -28,5 +28,9 @@ export class HeroeComponent {
 
 
 
+
+
+
+
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ContadorComponent } from './contador/contador.component';
+import { ContadorModule } from './contador/contador.module';
+
 import { HeroeComponent } from './Heroes/Heroe/heroe.component';
 import { ListadoComponent } from './listado/listado/listado.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContadorComponent,
+    
     HeroeComponent,
     ListadoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ContadorModule
   ],
   providers: [],
-  bootstrap: [HeroeComponent, ContadorComponent, ListadoComponent]
+  bootstrap: [AppComponent, HeroeComponent, ListadoComponent]
 })
 export class AppModule { }
